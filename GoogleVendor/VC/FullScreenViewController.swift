72feb0cd4eb2fd7a35e040a5497b6344d0b2d082
Lo <lolearningcode@gmail.com>
2019-09-28 08:50:44 -0400

    added zoom function

--- GoogleVendor/VC/FullScreenViewController.swift
+++ GoogleVendor/VC/FullScreenViewController.swift
@@ -20,25 +20,23 @@ class FullScreenViewController: UIViewController {
         }
     }
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var fullScreenImage: UIImageView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 6
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FullScreenViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return fullScreenImage
     }
-    */
-
 }
